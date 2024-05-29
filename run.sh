bb7754041921c556c1d39befde9f92abd633bb37
Parthiv Mothipati <pmothipati@gmail.com>
2024-05-29 10:26:42 -0700

    run

---
+++ run.sh
@@ -0,0 +1,5 @@
+cargo install mdbook
+cargo install --locked mdbook-svgbob
+cargo install --locked mdbook-i18n-helpers
+cargo install --locked --path mdbook-exerciser
+cargo install --locked --path mdbook-course
